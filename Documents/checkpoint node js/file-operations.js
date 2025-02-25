9861da8e7f803c97105ee8794f7f58bbfa75b5bf
Soukaina Mouradi Digital Project Manager | Minimalist Web Developer <soukainamouradi58@gmail.com>
2025-02-25 16:16:51 +0000

    Delete Documents directory

--- Documents/checkpoint node js/file-operations.js
+++
@@ -1,9 +0,0 @@
-// file-operations.js
-const fs = require('fs');
-
-// Create "welcome.txt" with the text "Hello Node"
-fs.writeFileSync('welcome.txt', 'Hello Node');
-
-// Read the content from "welcome.txt" and log it to the console
-const data = fs.readFileSync('welcome.txt', 'utf8');
-console.log(data);
